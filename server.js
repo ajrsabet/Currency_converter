82a927cb91818888b056f39cd91476a0f3b79c80
Adam Sabet <arishel@gmail.com>
2024-12-06 13:54:37 -0800

    Added an express server and converted html to ejs

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Set views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware to serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home Page', message: 'Welcome to My Website!' });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About Us', message: 'Learn more about us!' });
+});
+
+// 404 Page
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404', message: 'Page Not Found' });
+});
+
+// Start server
+// app.listen(3000, () => {
+//     console.log('Listening on http://localhost:3000');
+// });
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
